99a54d351812d188d80311e066dbaf0dcbe9555f
JuliusTapar <jcptapar05@gmail.com>
2024-08-12 21:57:58 +0800

    Update ui

--- src/Details.tsx
+++ src/Details.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Box, Grid } from "@mui/material";
+import { Box, Button, Container, Grid } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const Details = () => {
  const route = useParams();
@@ -22,60 +22,119 @@ const Details = () => {
  }, []);
 
  return (
-  <Grid
-   container
-   spacing={2}
-  >
+  <Container>
+   <Button sx={{ marginTop: "50px" }}>
+    <Link to="/">Back</Link>
+   </Button>
    <Grid
-    item
-    xs={6}
+    container
+    spacing={2}
    >
-    <Box
-     sx={{
-      display: "flex",
-      height: "100vh",
-      color: "#fff",
-      justifyContent: "center",
-      alignItems: "center",
-      mt: 1,
-      mb: 10,
-      gap: 3,
-      flexWrap: "wrap",
-     }}
+    <Grid
+     item
+     xs={6}
     >
-     <div>
-      <img
-       style={{ height: "300px", objectFit: "contain" }}
-       src={pokemon?.sprites.front_default}
-       alt=""
-      />
-     </div>
-    </Box>
-   </Grid>
+     <Box
+      sx={{
+       display: "flex",
+       height: "80vh",
+       color: "#fff",
+       justifyContent: "center",
+       alignItems: "center",
+       mt: 1,
+       mb: 10,
+       gap: 3,
+       flexWrap: "wrap",
+      }}
+     >
+      <div>
+       <img
+        style={{ height: "300px", objectFit: "contain" }}
+        src={pokemon?.sprites.front_default}
+        alt=""
+       />
+      </div>
+     </Box>
+    </Grid>
 
-   <Grid
-    item
-    xs={6}
-   >
-    <Box
-     sx={{
-      display: "flex",
-      height: "100vh",
-      color: "#fff",
-      justifyContent: "center",
-      alignItems: "center",
-      mt: 1,
-      mb: 10,
-      gap: 3,
-      flexWrap: "wrap",
-     }}
+    <Grid
+     item
+     xs={6}
     >
-     <div>
-      <h2 style={{ textTransform: "capitalize" }}>{pokemon?.name}</h2>
-     </div>
-    </Box>
+     <Box
+      sx={{
+       display: "flex",
+       height: "80vh",
+       color: "#fff",
+       justifyContent: "center",
+       alignItems: "center",
+       mt: 1,
+       mb: 10,
+       gap: 3,
+       flexWrap: "wrap",
+      }}
+     >
+      <div>
+       <h2 style={{ textTransform: "capitalize" }}>{pokemon?.name}</h2>
+       <p style={{ textTransform: "capitalize" }}>{pokemon?.id}</p>
+       <p style={{ textTransform: "capitalize" }}>Height: {pokemon?.name}</p>
+
+       {pokemon?.types.map((item: any) => (
+        <Button
+         disabled
+         key={item.slot}
+         size="small"
+         style={{
+          color: "#fff",
+          borderRadius: "30px",
+          fontSize: "10PX",
+          background: `
+        ${
+         item.type.name == "fire"
+          ? "red"
+          : item.type.name == "water"
+          ? "blue"
+          : item.type.name == "grass"
+          ? "green"
+          : item.type.name == "fighting"
+          ? "brown"
+          : item.type.name == "normal"
+          ? ""
+          : item.type.name == "ground"
+          ? "orange"
+          : item.type.name == "physic"
+          ? "teal"
+          : item.type.name == "rock"
+          ? "grey"
+          : item.type.name == "poison"
+          ? "violet"
+          : item.type.name == "electric"
+          ? "yellow"
+          : item.type.name == "ice"
+          ? "indigo"
+          : item.type.name == "bug"
+          ? "black"
+          : item.type.name == "dragon"
+          ? "yellowgreen"
+          : item.type.name == "fairy"
+          ? "aero"
+          : item.type.name == "normal"
+          ? "cyan"
+          : item.type.name == "ghost"
+          ? "bronze"
+          : ""
+        }
+        `,
+         }}
+        >
+         {item.type.name}
+        </Button>
+       ))}
+      </div>
+     </Box>
+    </Grid>
    </Grid>
-  </Grid>
+  </Container>
  );
 };
 
--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { Button } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -17,24 +18,70 @@ export default function Pokemon({ pokemon }: any) {
      component="img"
      alt="green iguana"
      height="140"
-     image={pokemon.image}
+     image={pokemon?.image}
      sx={{ objectFit: "contain" }}
     />
     <CardContent>
      <Typography
-      gutterBottom
       variant="h5"
       component="div"
       color="#fff"
-      style={{ textAlign: "center" }}
+      style={{ textAlign: "center", textTransform: "uppercase" }}
      >
-      {pokemon.name}
+      {pokemon?.name}
      </Typography>
     </CardContent>
     <CardActions>
-     {/* {types.map((item) => (
-     <Button size="small">{item}</Button>
-    ))} */}
+     {pokemon?.types.map((item: any) => (
+      <Button
+       key={item.slot}
+       size="small"
+       style={{
+        color: "#fff",
+        borderRadius: "30px",
+        fontSize: "10PX",
+        background: `
+        ${
+         item.type.name == "fire"
+          ? "red"
+          : item.type.name == "water"
+          ? "blue"
+          : item.type.name == "grass"
+          ? "green"
+          : item.type.name == "fighting"
+          ? "brown"
+          : item.type.name == "normal"
+          ? ""
+          : item.type.name == "ground"
+          ? "orange"
+          : item.type.name == "physic"
+          ? "teal"
+          : item.type.name == "rock"
+          ? "grey"
+          : item.type.name == "poison"
+          ? "violet"
+          : item.type.name == "electric"
+          ? "yellow"
+          : item.type.name == "ice"
+          ? "indigo"
+          : item.type.name == "bug"
+          ? "black"
+          : item.type.name == "dragon"
+          ? "yellowgreen"
+          : item.type.name == "fairy"
+          ? "aero"
+          : item.type.name == "normal"
+          ? "cyan"
+          : item.type.name == "ghost"
+          ? "bronze"
+          : ""
+        }
+        `,
+       }}
+      >
+       {item.type.name}
+      </Button>
+     ))}
     </CardActions>
    </Card>
   </Link>
